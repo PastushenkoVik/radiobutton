83772af1d887207d1a0e83d72f7e4a621cf5a673
Viktor Pastushenko <pastushenko.vik@gmail.com>
2020-05-03 14:24:47 +0300

    Updates

---
+++ precache-manifest.ff91e5c75c8de94977268842d4ad95fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e48468b23b3c679a8711ce4311a2a1df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c1910f5a28994212a43",
+    "url": "/static/css/main.1b5510b7.chunk.css"
+  },
+  {
+    "revision": "e8840e5158643350d3f4",
+    "url": "/static/js/2.edf58a73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.edf58a73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c1910f5a28994212a43",
+    "url": "/static/js/main.f1d90237.chunk.js"
+  },
+  {
+    "revision": "2de6a86b6e46165a2f07",
+    "url": "/static/js/runtime-main.8a356cd9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
